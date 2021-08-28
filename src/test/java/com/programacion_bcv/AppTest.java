4f46fa7091173696c5b7f02ed625686956d55f93
Confaa <juanconfaa@gmail.com>
2021-08-28 14:29:45 -0300

    Inicio de repositorio y logica signo del zodiaco

---
+++ src/test/java/com/programacion_bcv/AppTest.java
@@ -0,0 +1,20 @@
+package com.programacion_bcv;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
